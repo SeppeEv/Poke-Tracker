bf916ef2c86b3f4635062c3d4260268d327004ef
SeppeEv <seppe.everaet@student.hogent.be>
2023-12-26 17:40:37 +0100

    add bg image, styling and first resource strings

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +15,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,22 +29,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             ApplicationTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    GreetingText(message = "Hello user!", from = "From Seppe")
+                    GreetingImage(message = getString(R.string.hello_user), from = getString(R.string.from_seppe))
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
 }
 
 @Composable
@@ -65,10 +60,32 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.lugia_background)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+                .fillMaxSize(),
+            alpha = 0.5f,
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ApplicationTheme {
-        GreetingText(message = "Hello user!", from = "From Seppe")
+        GreetingImage(message = "Hello user", from = "From Seppe")
     }
 }
