3a6099c4e0686c0df34fcaf03f5cac6f55ff8d4b
SeppeEv <seppe.everaet@student.hogent.be>
2023-12-27 12:03:08 +0100

    create homepage

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -9,23 +9,45 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -40,16 +62,24 @@ import androidx.compose.ui.unit.sp
 import com.example.application.ui.theme.ApplicationTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ApplicationTheme {
-                Surface(
-                    color = MaterialTheme.colorScheme.background,
-                ) {
-                    GreetingImage(message = getString(R.string.hello_user), from = getString(R.string.from_seppe))
-                }
-            }
+            val windowSizeClass = calculateWindowSizeClass(this)
+            PokeTrackerApp(windowSizeClass)
+        }
+    }
+}
+
+@Composable
+fun PokeTrackerApp(windowSize: WindowSizeClass) {
+    when (windowSize.widthSizeClass) {
+        WindowWidthSizeClass.Compact -> {
+            PokeTrackerAppPortrait()
+        }
+        WindowWidthSizeClass.Expanded -> {
+            PokeTrackerAppLandscape()
         }
     }
 }
@@ -210,6 +240,270 @@ fun GenerationCardPreview() {
             Modifier.padding(8.dp),
         )
     }
+}
+
+@Composable
+fun TypeElementRow(
+    modifier: Modifier = Modifier,
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier,
+    ) {
+        val typesData = listOf(
+            "grass",
+            "fire",
+            "water",
+            "bug",
+            "normal",
+            "poison",
+            "electric",
+            "ground",
+        )
+        items(typesData) { item ->
+            TypeElement(
+                drawable = R.drawable.grass_sprite,
+                text = R.string.grass_type,
+            )
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .verticalScroll(rememberScrollState()),
+    ) {
+        Spacer(Modifier.height(16.dp))
+        Searchbar(Modifier.padding(horizontal = 16.dp))
+        HomeSection(title = R.string.types) {
+            TypeElemntRow()
+        }
+        HomeSection(title = R.string.generations) {
+            GenerationCardGrid()
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE, heightDp = 180)
+@Composable
+fun HomeScreenContentPreview() {
+    ApplicationTheme {
+        HomeScreen()
+    }
+}
+
+@Composable
+fun GenerationCardGrid(
+    modifier: Modifier = Modifier,
+) {
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(3),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier
+            .height(240.dp),
+    ) {
+        val generationsData = listOf(
+            "generation_1",
+            "generation_2",
+            "generation_3",
+            "generation_4",
+            "generation_5",
+            "generation_6",
+            "generation_7",
+            "generation_8",
+        )
+        items(generationsData) { item ->
+            GenerationCard(
+                drawable = R.drawable.pokemon_red__1_,
+                text = R.string.generation_1,
+                Modifier.height(80.dp),
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun GenerationCardGridPreview() {
+    ApplicationTheme {
+        GenerationCardGrid()
+    }
+}
+
+@Preview
+@Composable
+fun TypeElemntRow() {
+    ApplicationTheme {
+        TypeElementRow()
+    }
+}
+
+@Composable
+fun BottomNavigation(modifier: Modifier = Modifier) {
+    NavigationBar(
+        modifier = modifier,
+    ) {
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = null,
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(R.string.bottom_navigation_home),
+                )
+            },
+            selected = true,
+            onClick = {},
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null,
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(R.string.bottom_navigation_profile),
+                )
+            },
+            selected = false,
+            onClick = {},
+        )
+    }
+}
+
+@Preview
+@Composable
+fun BottomNavigationPreview() {
+    ApplicationTheme {
+        BottomNavigation()
+    }
+}
+
+@Composable
+fun HomeSection(
+    @StringRes title: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        Column(modifier) {
+            Text(
+                text = stringResource(title),
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier
+                    .paddingFromBaseline(top = 40.dp, bottom = 16.dp)
+                    .padding(horizontal = 16.dp),
+            )
+            content()
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun HomeSectionPreview() {
+    ApplicationTheme {
+        HomeSection(R.string.types) {
+            TypeElementRow()
+        }
+    }
+}
+
+@Composable
+fun PokeTrackerAppPortrait() {
+    ApplicationTheme {
+        Scaffold(
+            bottomBar = {
+                BottomNavigation()
+            },
+        ) { padding ->
+            HomeScreen(Modifier.padding(padding))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PokeTrackerAppPortraitPreview() {
+    PokeTrackerAppPortrait()
+}
+
+@Composable
+fun PokeTrackerNavigationRail(modifier: Modifier = Modifier) {
+    NavigationRail(
+        modifier = modifier.padding(start = 8.dp, end = 8.dp),
+        containerColor = MaterialTheme.colorScheme.background,
+    ) {
+        Column(
+            modifier = modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Home,
+                        contentDescription = null,
+                    )
+                },
+                label = {
+                    Text(stringResource(R.string.bottom_navigation_home))
+                },
+                selected = true,
+                onClick = {},
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.AccountCircle,
+                        contentDescription = null,
+                    )
+                },
+                label = {
+                    Text(stringResource(R.string.bottom_navigation_profile))
+                },
+                selected = false,
+                onClick = {},
+            )
+        }
+    }
+}
+
+@Composable
+fun PokeTrackerAppLandscape() {
+    ApplicationTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            Row {
+                PokeTrackerNavigationRail()
+                HomeScreen()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PokeTrackerAppLandscapePreview() {
+    PokeTrackerAppLandscape()
+}
+
+@Preview
+@Composable
+fun PokeTrackerNavigationRailPreview() {
+    PokeTrackerNavigationRail()
 }
 
 @Preview(showBackground = true)
